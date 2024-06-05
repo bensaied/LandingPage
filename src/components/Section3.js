0dda0e653a2af645ca6bb6dc3f5216e8f4d2d498
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-06-05 18:12:16 +0200

    feat: Setup the third section

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -1,16 +1,65 @@
+// export default Section3;
 import React from "react";
-import styles from "../../styles/Section1.module.css";
+import styles from "../../styles/Section3.module.css";
 
 const Section3 = () => {
   return (
     <section id="section3" className={styles.section}>
-      <div className={styles.textOverlay}>
-        <h1>Lorem ipsum dolor sit amet, consec</h1>
-        <p>
-          Aliquam eu malesuada turpis, eu interdum nibh. Etiam tristique erat in
-          ligula consequat malesuada.
+      <img
+        src="/images/3.png"
+        alt="Background"
+        className={styles.backgroundImage}
+      />
+      <div className={styles.verticalLine}></div>
+      <div className={styles.verticalLine}></div>
+      <div className={styles.verticalLine}></div>
+
+      <div className={styles.part}>
+        <button className={styles.iconButton}>
+          <span className={styles.plusIcon}>+</span>
+        </button>
+        <h2 className={styles.title}>Financial statemetns</h2>
+        <p className={styles.description}>
+          Morbi purus libero, elementum nec gravida ac, commondo at erat. Etiam
+          porta ipsum sed diam aliquam, ruturum tincidunt metus mattis. Morbi
+          purus libero, elementum nec
         </p>
-        <p>Praesent posuere vestibulum neque ac posuere.</p>
+      </div>
+
+      <div className={styles.part}>
+        <button className={styles.iconButton}>
+          <span className={styles.plusIcon}>+</span>
+        </button>
+        <h2 className={styles.title}>Press releases</h2>
+        <p className={styles.description}>
+          Morbi purus libero, elementum nec gravida ac, commondo at erat. Etiam
+          porta ipsum sed diam aliquam, ruturum tincidunt metus mattis. Morbi
+          purus libero, elementum nec
+        </p>{" "}
+      </div>
+
+      <div className={styles.part}>
+        <button className={styles.iconButton}>
+          <span className={styles.plusIcon}>+</span>
+        </button>
+        <h2 className={styles.title}>Webcast links</h2>
+        <p className={styles.description}>
+          Morbi purus libero, elementum nec gravida ac, commondo at erat. Etiam
+          porta ipsum sed diam aliquam, ruturum tincidunt metus mattis. Morbi
+          purus libero, elementum nec
+        </p>{" "}
+      </div>
+
+      <div className={styles.part}>
+        <button className={styles.iconButton}>
+          <span className={styles.plusIcon}>+</span>
+        </button>
+        <h2 className={styles.title}>Corporate governance</h2>
+        <p className={styles.description}>
+          Morbi purus libero, elementum nec gravida ac, commondo at erat. Etiam
+          porta ipsum sed diam aliquam, ruturum tincidunt metus mattis. Morbi
+          purus libero, elementum nec
+        </p>{" "}
       </div>
     </section>
   );
