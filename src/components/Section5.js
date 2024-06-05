da320e84449d8c769092f2a15e23571da9a4eb26
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-06-05 21:31:22 +0200

    feat: Setup the fifth section

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -1,16 +1,33 @@
 import React from "react";
-import styles from "../../styles/Section1.module.css";
+import styles from "../../styles/Section5.module.css";
 
 const Section5 = () => {
   return (
     <section id="section5" className={styles.section}>
-      <div className={styles.textOverlay}>
-        <h1>Lorem ipsum dolor sit amet, consec</h1>
-        <p>
-          Aliquam eu malesuada turpis, eu interdum nibh. Etiam tristique erat in
-          ligula consequat malesuada.
-        </p>
-        <p>Praesent posuere vestibulum neque ac posuere.</p>
+      <div className={styles.formBox}>
+        <h3>Any questions?</h3>
+        <h2>Let's talk today!</h2>
+        <input
+          type="text"
+          id="name"
+          className={styles.inputField}
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          id="email"
+          className={styles.inputField}
+          placeholder="Email"
+        />
+        <textarea
+          id="message"
+          className={styles.inputField}
+          placeholder="Message"
+          rows="6"
+        ></textarea>
+        <button type="submit" className={styles.button}>
+          Submit
+        </button>
       </div>
     </section>
   );
