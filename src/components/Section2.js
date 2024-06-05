16da8758c5d73a28f853746eca4c1c601986839d
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-06-05 14:05:59 +0200

    feat: Devide page into sections

---
+++ src/components/Section2.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../../styles/Section1.module.css";
+
+const Section2 = () => {
+  return (
+    <section id="section2" className={styles.header}>
+      <div className={styles.textOverlay}>
+        <h1>Lorem ipsum dolor sit amet, consec</h1>
+        <p>
+          Aliquam eu malesuada turpis, eu interdum nibh. Etiam tristique erat in
+          ligula consequat malesuada.
+        </p>
+        <p>Praesent posuere vestibulum neque ac posuere.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Section2;
---
+++ src/components/Section3.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../../styles/Section1.module.css";
+
+const Section3 = () => {
+  return (
+    <section id="section3" className={styles.header}>
+      <div className={styles.textOverlay}>
+        <h1>Lorem ipsum dolor sit amet, consec</h1>
+        <p>
+          Aliquam eu malesuada turpis, eu interdum nibh. Etiam tristique erat in
+          ligula consequat malesuada.
+        </p>
+        <p>Praesent posuere vestibulum neque ac posuere.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Section3;
